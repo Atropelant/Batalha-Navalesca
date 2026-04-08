18e35469acaad722e4eff871e20c445cbc22ab7f
Felipe Bereta <fbereta1@gmail.com>
2026-04-07 22:17:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function tabela() {
             tab[l][c] = imagens[indice];
             const img = document.createElement("img");
             // tiroteioo
-            img.src = "src/Fire-icon.png";
+            img.src = "Fire-icon.png";
             img.width = 50;
             img.id = l + "-" + c;
             img.onclick = function () {
